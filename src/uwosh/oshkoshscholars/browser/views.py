eec2a8df246b702ed9950f14d09d48d2f0fe3bc1
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2016-05-19 15:12:17 -0500

    handle case when user not logged in, fixes #10

--- src/uwosh/oshkoshscholars/browser/views.py
+++ src/uwosh/oshkoshscholars/browser/views.py
@@ -1,6 +1,6 @@
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-
+from plone import api
 
 class StudentView(BrowserView):
     pass
@@ -15,4 +15,9 @@ class AddSubmissionHomeFolder(BrowserView):
      def __call__(self):
          pm = getToolByName(self, 'portal_membership')
          home_url = pm.getHomeUrl()
-         self.request.response.redirect(home_url + '/++add++StudentSubmission')
+         if home_url:
+             self.request.response.redirect(home_url + '/++add++StudentSubmission')
+         else:
+             portal_url = api.portal.get().absolute_url()
+             add_submission_url = portal_url + '/add_submission_in_home_folder'
+             self.request.response.redirect(portal_url + '/login_form?came_from=%s' % add_submission_url)
